0456188f31ac87fbe96ed9d8f1bf3d21c57be63b
Iuri Ciciliotti <iurimattosc@gmail.com>
2024-11-26 00:06:15 -0300

    Voltando ao normal

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -7,7 +7,7 @@ const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 // Função assíncrona para buscar todos os posts do banco de dados
  export async function getTodosPosts() {
     // Seleciona o banco de dados "Imersao-instabytes"
-    //const db = conexao.db("Imersao-instabytes");
+    const db = conexao.db("Imersao-instabytes");
     // Seleciona a coleção "posts" dentro do banco de dados
     const colecao = db.collection("posts");
     // Busca todos os documentos da coleção e retorna como um array
